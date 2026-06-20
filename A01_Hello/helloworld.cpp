6b25bcb8e8d1ad1041db8826774762cca730208d
José Amezcua <jjesusamezcualopez@gmail.com>
2026-06-20 12:46:21 -0600

    Hello_World

--- A01_Hello/helloworld.cpp
+++ A01_Hello/helloworld.cpp
@@ -6,3 +6,10 @@ int main (){
     cout<<"Hello World :D"<<endl; 
     return 0;
 }
+
+/* Estos primeros ejemplos serán sobre C++, cuando trabajamos con microcontroladores
+usamos otras librerías y otros metodos de impresion y entrada de datos, ojo con eso, más adeltante
+iré cambiando la forma de trabajo, por ahora, veran ejemplos basicos de C.
+
+(Si no uso acentos no es mala ortografia, es por el IDE)s
+*/
