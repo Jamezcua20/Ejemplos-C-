0759098d2a26bd03e9b613c0030cb7ce70957586
José Amezcua <jjesusamezcualopez@gmail.com>
2026-06-28 19:11:23 -0600

    Add mathematical function f(x,y) example

--- A06_raiz_elevado/calculo.c
+++ A06_raiz_elevado/calculo.c
@@ -1,20 +1,19 @@
 /* Programa para calcular función matematica f(x,y) = (sqrt(x))/ y^2-1
 Para usar raiz(sqrt) y potencia (pow) usar math.h
 */
-#include <iostream>
+#include <stdio.h>
 #include <math.h>
-using namespace std;
 
 int main(){
     float resultado = 0, x=0; 
     int y=0;
-    cout<<"Ingresar valor de X: ";
-    cin>>x;
-    cout<<"\nIngresar valor de y: ";
-    cin>>y;
+    printf("Ingresar valor de X: ");
+    scanf("%f", &x);
+    printf("\nIngresar valor de y: ");
+    scanf("%d", &y);
 
     resultado = sqrt(x)/(pow(y,2)-1);
-    cout<<"\nEl resultado es: "<<resultado;
+    printf("\nEl resultado es: %f\n", resultado);
 
     return 0;
 }
